483f944792275d324152ea1614868fe88322f057
kadelcode <kadelcode@gmail.com>
2025-05-12 22:03:02 +0100

    Modify routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,16 @@
 const express = require('express'); // Importing express for creating the router
 const router = express.Router(); // Creating a new router instance
-const { register, login, profile } = require('../controllers/authController'); // Importing the controller functions for handling authentication
+const { register, login, profile, logout } = require('../controllers/authController'); // Importing the controller functions for handling authentication
 const authMiddleware = require('../middleware/authMiddleware'); // Importing the authentication middleware
+const ROLES = require('../config/roles'); // Importing the roles configuration
 
 router.post('/register', register); // Route for user registration
 router.post('/login', login); // Route for user login
-router.get('/profile', authMiddleware(['admin', 'user']), profile); // Route for getting user profile, protected by authentication middleware
-router.get('/admin', authMiddleware('admin'), (req, res) => { // Route for admin access, protected by authentication middleware
+router.get('/profile', authMiddleware([ROLES.USER, ROLES.ADMIN]), profile); // Route for getting user profile, protected by authentication middleware
+router.get('/admin', authMiddleware([ROLES.ADMIN]), (req, res) => { // Route for admin access, protected by authentication middleware
     res.status(200).json({ message: 'Welcome Admin' }); // Sending a welcome message for admin access
-} // Sending a 200 OK response with the welcome message
-);
+}) // Sending a 200 OK response with the welcome message
+router.get('/logout', authMiddleware(), logout); // Sending a 200 OK response with the logout message
+
 
 module.exports = router; // Exporting the router to be used in other parts of the application
