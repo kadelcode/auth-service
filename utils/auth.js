2762f591489d437ea99b54ecbb3017909f3b7ad7
kadelcode <kadelcode@gmail.com>
2025-05-16 17:04:00 +0100

    Utility function to set authentication cookie

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+const setAuthCookie = (res, token) => {
+    res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'None',
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    });
+}
+
+module.exports = { setAuthCookie };
