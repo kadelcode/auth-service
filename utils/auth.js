2ee7f05d88911dc4b2f8ceb57301ee9027a22b96
kadelcode <kadelcode@gmail.com>
2025-05-17 20:12:21 +0100

    Update code

--- utils/auth.js
+++ utils/auth.js
@@ -1,8 +1,8 @@
 const setAuthCookie = (res, token) => {
     res.cookie('token', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+        secure: true,
+        sameSite: "None",
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     });
 }
